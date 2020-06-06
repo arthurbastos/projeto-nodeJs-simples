485dd6cabe930109e424002109e238c1898dc5e5
arthurbastos <arthurcoelho.27@gmail.com>
2020-06-05 22:15:33 -0300

    Comitando o projeto

---
+++ src/database/db.js
@@ -0,0 +1,74 @@
+// importar a dependencia do SqlLite
+const sqlite3 = require("sqlite3").verbose()
+
+// Criar o objeto que ira fazer operações no banco de dados
+const db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db
+
+db.serialize(() => {
+    //Criar tabela
+    /* db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?); 
+    `
+
+    const values = [
+        "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "Papersider",
+        "Guilherme Gemballa, Jardim América",
+        "Nº 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Papeis e Papelão"
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com secusso")
+        console.log(this)
+    } */
+
+    //db.run(query, values, afterInsertData)
+
+    /* db.all(`SELECT * FROM places`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+    }) */
+
+    /* db.run(`DELETE FROM places WHERE id = ?`, [6], function (err) {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("Registro deletado com sucesso!")
+    }) */
+
+})
---
+++ src/server.js
@@ -0,0 +1,92 @@
+const express = require("express")
+const server = express()
+
+const db = require("./database/db.js")
+
+//Configurar a pasta publica
+server.use(express.static("public"))
+
+server.use(express.urlencoded({ extended: true }))
+
+//Utulizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+//Configurar a reposta do servidor
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+//Esse serve para configurar as rotas 
+server.get("/create-point", (req, res) => {
+
+    console.log(req.query)
+
+    return res.render("create-point.html")
+})
+
+server.post("/create-point", (req, res) => {
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?); 
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.itens
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro")
+        }
+
+        console.log("Cadastrado com secusso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+
+    if (search == "") {
+        return res.render("search-results.html", { total: 0 })
+    } else {
+        db.all(`SELECT * FROM places WHERE city like '%${search}%'`, function (err, rows) {
+            if (err) {
+                return console.log(err)
+            }
+
+            const total = rows.length
+
+            return res.render("search-results.html", { places: rows, total: total })
+        })
+    }
+})
+
+
+//ligar o servidor
+server.listen(3000)
